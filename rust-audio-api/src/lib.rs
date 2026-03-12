ece0ad607332942aabd34998e85aa19e6ea70cee
leon123858 <a0970785699@gmail.com>
2026-03-12 23:46:01 +0800

    rm no used code

--- rust-audio-api/src/lib.rs
+++ rust-audio-api/src/lib.rs
@@ -6,18 +6,3 @@ pub mod types;
 pub use context::AudioContext;
 pub use graph::{GraphBuilder, NodeId, StaticGraph};
 pub use types::{AUDIO_UNIT_SIZE, AudioUnit};
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
